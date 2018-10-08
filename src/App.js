4713307a68370f762ce81018fb977450db3ed4a1
Mathis Hofer <hofer@puzzle.ch>
2018-10-08 11:59:09 +0200

    Add howler.js and implement example audio stream playback

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Howl } from 'howler';
 import './App.css';
 
+const stationUrl = 'http://stream.srg-ssr.ch/drs1/mp3_128.m3u';
+const streamUrl = 'http://stream.srg-ssr.ch/m/regi_be_fr_vs/mp3_128';
+const FADE_TIME = 300;
+
 class App extends Component {
+  sound = null;
+  soundId = null;
+
+  togglePlayback() {
+    if (this.isPlaying) {
+      this.stop();
+    } else {
+      this.play();
+    }
+  }
+
+  play() {
+    this.stop().then(() => {
+      this.sound = new Howl({
+        src: streamUrl,
+        html5: true,
+        volume: 0
+      });
+      this.soundId = this.sound.play();
+      this.sound.fade(0, 1, FADE_TIME);
+    });
+  }
+
+  stop() {
+    return new Promise(resolve => {
+      this.sound && this.sound.fade(1, 0, FADE_TIME);
+      setTimeout(() => {
+        this.sound && this.sound.unload();
+        this.sound = null;
+        this.soundId = null;
+        resolve();
+      }, this.isPlaying ? FADE_TIME : 0);
+    });
+  }
+
+  get isPlaying() {
+    return this.soundId && this.sound && this.sound.playing(this.soundId);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={this.togglePlayback.bind(this)}>Play/Stop</button>
         </header>
       </div>
     );
